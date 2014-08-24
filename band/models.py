ce4e261c6c3e42c5c2ea11b71bf3ac7afbdd2fcc
Bob Schmertz <rschmertz@42six.com>
2014-08-24 19:22:14 -0400

    Add member model, register app

--- band/models.py
+++ band/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    instrument = models.CharField(max_length=20)
+    join_date = models.DateTimeField('date published')
+
