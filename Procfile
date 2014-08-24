769e2b3ae90cd3742d8b159e5bfdbee7037ebb35
Bob Schmertz <rschmertz@42six.com>
2014-08-23 20:04:16 -0400

    Prep for deployent in Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn painless.wsgi --log-file -
--- painless/settings.py
+++ painless/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -19,12 +19,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&#vh$y=14n(!&ny9w^+gcr(6hkqc*mj7y$h##en8ed26t%*ils'
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#####
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+# Allow all host headers -- per heroku-getting-started
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,10 +62,12 @@ WSGI_APPLICATION = 'painless.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'init-default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    # Parse database configuration from $DATABASE_URL
+    'default': dj_database_url.config()
 }
 
 # Internationalization
@@ -79,4 +87,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles' # from heroku-getting-started
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
